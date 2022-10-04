eea6eef33f38902d3733b8f6df25cc4f9321ff4f
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-04 22:34:53 +0200

    meeting and wishlist updated

--- meeting.py
+++ meeting.py
@@ -1,4 +1,9 @@
 import this
 
 attendees = ["Rachel", "Michel", "Oliver"]
-print("There are", len(attendees), "attendees currently")
+attendees.append("Jack")
+attendees.extend(["Ben", "Jane"])
+optional_initees = ["Clarck", "Zoe"]
+potentional_attendees = attendees + optional_initees
+print("There are", len(potentional_attendees), "attendees currently")
+print(potentional_attendees)
