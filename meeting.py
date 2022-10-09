215575a71aa066556f33d86e4a1445393dd151d9
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-09 11:13:28 +0200

    added "join"

--- meeting.py
+++ meeting.py
@@ -3,7 +3,12 @@ import this
 attendees = ["Rachel", "Michel", "Oliver"]
 attendees.append("Jack")
 attendees.extend(["Ben", "Jane"])
-optional_initees = ["Clarck", "Zoe"]
-potentional_attendees = attendees + optional_initees
+optional_invitees = ["Clarck", "Zoe"]
+potentional_attendees = attendees + optional_invitees
 print("There are", len(potentional_attendees), "attendees currently")
 print(potentional_attendees)
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_invitees)
+print("To:  " + to_line)
+print("CC:  " + cc_line )
