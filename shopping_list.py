92cfa21d4f23d247ed746c73f097f74eaf35c8e4
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-09 17:54:37 +0200

    created shopping list (break-continue)

---
+++ shopping_list.py
@@ -0,0 +1,15 @@
+def show_help():
+    print("What should we pick up at the store?")
+    print("""""
+    Enter 'DONE' to stop adding items.
+    Enter 'HELP' for this help.
+    """)
+show_help()
+while True:
+    new_item = input("> ")
+
+    if new_item == 'DONE':
+       break
+    elif new_item == 'HELP':
+        show_help()
+        continue
